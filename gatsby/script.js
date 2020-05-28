bc384424f5608668e1c3ce398a360dc260d32e9c
andy <andy.ukpe@outlook.com>
2020-05-28 16:50:45 +0200

    Crretto esercizio Gatsby e aggiunto ciclo while

--- gatsby/script.js
+++ gatsby/script.js
@@ -1,3 +1,25 @@
+// // In un array sono contenuti i nomi degli invitati
+// // alla festa del grande Gatsby,
+// // chiedi all’utente il suo nome e comunicagli se
+// // può partecipare o no alla festa.
+//
+// // LISTA INVITATI ALLA festa
+// var invitatiFesta = ['Valerie', 'Ernesto', 'Joshua', 'Melanie', 'Franco'];
+// // CHIEDERE ALL'UTENTE IL nome
+// var nomeUtente = prompt('Inserisci il tuo nome per vedere se Gatsby ti ha incluso nella lista invitati!');
+// console.log(nomeUtente);
+// // var messaggio
+// var messaggio = 'Mi spiace, non puoi partecipare';
+//
+// // confronto lista invitati
+// for (var i = 0; i < invitatiFesta.length; i++) {
+//   if(invitatiFesta[i] == nomeUtente){
+//     messaggio = 'Sei in lista';
+//     console.log(invitatiFesta[i] == nomeUtente);
+//
+// }
+// alert (messaggio);
+
 // In un array sono contenuti i nomi degli invitati
 // alla festa del grande Gatsby,
 // chiedi all’utente il suo nome e comunicagli se
@@ -5,21 +27,28 @@
 
 // LISTA INVITATI ALLA festa
 var invitatiFesta = ['Valerie', 'Ernesto', 'Joshua', 'Melanie', 'Franco'];
+
 // CHIEDERE ALL'UTENTE IL nome
 var nomeUtente = prompt('Inserisci il tuo nome per vedere se Gatsby ti ha incluso nella lista invitati!');
 console.log(nomeUtente);
-// var messaggio
-var messaggio;
+
+// var presenza in lista
+var presenteInLista = false;
+var i = 0;
 
 // confronto lista invitati
-for (var i = 0; i < invitatiFesta.length; i++) {
-  if(invitatiFesta[i] == nomeUtente){
-    messaggio = 'Sei in lista';
-    console.log(invitatiFesta[i] == nomeUtente);
-  } else {
-    massaggio = 'Mi spiace, non puoi partecipare';
-    console.log(invitatiFesta[i] != nomeUtente);
-   }
+while (presenteInLista != true && i < invitatiFesta.length) {
 
+  if(invitatiFesta[i] == nomeUtente){
+    presenteInLista = true;
+  }
+  console.log(invitatiFesta[i]);
+  i++
+};
+
+if(presenteInLista === true){
+  console.log('sei invitato');
 }
-alert (messaggio);
+else {
+  console.log('non sei in lista');
+};
--- jSnack 2/script.js
+++ jSnack 2/script.js
@@ -4,15 +4,36 @@
 
 // chiedere all'utente di inserire 5 volte un numero
 // variabili
-var numero = [];
-console.log(numero);
+// var numero = [];
+// console.log(numero);
+//
+//   numeroUtente = prompt('inserisci un numero');
+//   console.log(numeroUtente);
+//
+// // INSERIRE NOMI NELL'ARRAY
+// for( i = 0 ; i < 5 ; i++){
+//   numero.push(numeroUtente);
+//   console.log(numero[i]);
+//   // if(numero[i])
+//   }
 
-  numeroUtente = prompt('inserisci un numero');
-  console.log(numeroUtente);
+  // Il software deve chiedere per 5 volte all’utente di inserire un numero.
+  //  Il programma stampa la somma di tutti i numeri inseriti.
+  //  Esegui questo programma in due versioni, con il for e con il while.
 
-// INSERIRE NOMI NELL'ARRAY
-for( i = 0 ; i < 5 ; i++){
-  numero.push(numeroUtente);
-  console.log(numero[i]);
-  // if(numero[i])
-  }
+  // variabili
+  somma = 0;
+  // il software chiede per 5 volte di inserire un numero
+  for( var i = 0 ; i < 5 ; i++){
+    // CHIEDERE IL NUMERO ALL'UTENTE
+    var numeroUtente = prompt('inserisci un numero');
+    //inserire il numeroUtente nell'array
+    numeroUtente.toString();
+    numeri.push(numeroUtente[i]);
+
+      }
+    } else{
+      numeroUtente = prompt('inserisci un numero');
+    }
+    console.log(numeroUtente)
+    console.log(numeri);
